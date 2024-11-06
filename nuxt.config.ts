8a76abdaa798d8480caa463cec8e5a7885e1b780
ivan119 <ivankelava91@gmail.com>
2024-11-06 21:50:37 +0100

    feat: slide-animation for layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    layoutTransition: {
+      name: "slide",
+      mode: "out-in",
+    },
+  },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
-  ssr: true,
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@tresjs/nuxt"],
   postcss: {
     plugins: {
       tailwindcss: {},
