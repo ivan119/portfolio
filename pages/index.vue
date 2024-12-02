29a93ebf1a0233cd860d1833dc7a801f2b380b90
ivan119 <ivankelava91@gmail.com>
2024-12-02 21:12:40 +0100

    feat: optimize

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,34 @@
 <script setup>
-import BaseButton from '~/components/BaseButton.vue'
+// Lazy load BaseButton since it's not needed immediately
+const BaseButton = defineAsyncComponent(
+  () => import("~/components/BaseButton.vue"),
+);
 </script>
 
 <template>
   <div class="flex flex-col gap-6">
-    <h1 class="text-3xl md:text-4xl font-bold mb-3 no-animation">Ivan Kelava</h1>
-    
-    <article class="prose slide-enter-active flex flex-col gap-4 dark:prose-invert">
-      <h4 class="text-xl md:text-2xl mb-2 text-gray-800 dark:text-gray-300">
-          Hey, I am Ivan Kelava, senior frontend developer.
+    <h1 class="text-3xl md:text-4xl font-bold mb-3">Ivan Kelava</h1>
+
+    <article class="flex flex-col gap-4 text-gray-800 dark:text-gray-300">
+      <h4 class="text-xl md:text-2xl mb-2">
+        Hey, I am Ivan Kelava, senior frontend developer.
       </h4>
-        
+
       <p class="flex items-center gap-1">
-        Currently working at 
-        <BaseButton 
-          href="https://gauss.hr" 
+        Currently working at
+        <BaseButton
+          href="https://gauss.hr"
           external
           icon="/images/gauss.svg"
+          loading="lazy"
         >
           Gauss
         </BaseButton>
         as team lead developer
       </p>
-      
+
       <div class="flex">
-        <BaseButton
-          to="/about"
-          variant="primary"
-        >
-          About
-        </BaseButton>
+        <BaseButton to="/about" variant="primary"> About </BaseButton>
       </div>
     </article>
   </div>
