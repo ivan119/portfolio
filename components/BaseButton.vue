dca1781db7c4b058b3f4e3450c2977fd775215d7
ivan119 <ivankelava91@gmail.com>
2025-02-13 21:34:17 +0100

    chore: expand BaseButton

--- components/BaseButton.vue
+++ components/BaseButton.vue
@@ -7,7 +7,7 @@ interface Props {
   href?: string;
   external?: boolean;
   alt?: string;
-  variant?: "default" | "primary";
+  variant?: "default" | "primary" | "link";
   loading?: "lazy" | "eager";
 }
 
@@ -19,8 +19,9 @@ const props = withDefaults(defineProps<Props>(), {
 });
 
 const buttonClasses = computed(() => [
+  props.variant === "link" ? "relative font-bold underline" : "",
   props.variant === "default"
-    ? " text-sm  py-1 rounded bg-gray-100  px-2 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700"
+    ? "font-bold text-sm  py-1 rounded bg-gray-100  px-2 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700"
     : "",
   props.variant === "primary"
     ? "text-2xl text-teal-500 hover:text-teal-600"
@@ -28,21 +29,39 @@ const buttonClasses = computed(() => [
 ]);
 
 const component = computed(() => {
-  if (props.to || props.href) return defineNuxtLink({});
-  return "button";
+  if (props.to) return defineNuxtLink({});
+  return props.href ? "a" : "button";
 });
 </script>
 
 <template>
   <component
     :is="component"
-    :to="to || href"
+    :to="to"
+    :href="href"
     :target="external ? '_blank' : undefined"
     :rel="external ? 'opener referrer' : undefined"
-    class="transition-colors ease-linear duration-[369] inline-flex items-center"
+    class="transition-colors ease-linear duration-[369] inline-flex items-center relative group"
     :class="buttonClasses"
   >
     <slot name="icon" />
     <slot />
+    <span v-if="props.variant === 'link'" class="underline-effect"></span>
   </component>
 </template>
+
+<style scoped>
+.underline-effect {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 0;
+  height: 0.04em;
+  background-color: currentColor;
+  transition: width 0.3s ease-in-out;
+}
+
+.group:hover .underline-effect {
+  width: 100%;
+}
+</style>
