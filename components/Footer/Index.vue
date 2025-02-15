6962219d74e4dbfceec6bd5ab24fe529a51cb0f0
ivan119 <ivankelava91@gmail.com>
2025-02-15 19:49:10 +0100

    chore: update date

--- components/Footer/Index.vue
+++ components/Footer/Index.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+const dateNow = new Date();
+</script>
+
 <template>
   <footer class="text-left py-4 text-gray-600 dark:text-gray-400">
-    &copy; Ivan Kelava • CC BY-NC-SA 4.0 2024
+    &copy; Ivan Kelava • CC BY-NC-SA 4.0 {{ dateNow.getFullYear() }}
   </footer>
 </template>
