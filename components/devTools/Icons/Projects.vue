0eecc521282cd3f55a0eeed05185dddc55fd109f
ivan119 <ivankelava91@gmail.com>
2025-02-15 15:34:44 +0100

    feat: project icon

---
+++ components/devTools/Icons/Projects.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="w-5 h-5"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+    <path d="M8 7h8"></path>
+    <path d="M8 12h8"></path>
+    <path d="M8 17h8"></path>
+  </svg>
+</template>
