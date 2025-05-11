5eb5b2cac612945f32eca7fac55db903ba106601
Your Name <you@example.com>
2025-05-11 12:44:41 +0200

    chore: banner

--- components/UI/Banner.vue
+++ components/UI/Banner.vue
@@ -10,11 +10,16 @@ const props = defineProps({
     required: true,
     default: "Insert Description Here",
   },
+  firstTagIsH1: {
+    type: Boolean,
+    default: true,
+    required: false,
+  },
 });
 </script>
 
 <template>
-  <section class="relative">
+  <section class="relative p-4 md:p-8">
     <div
       class="absolute inset-0 bg-gradient-to-br from-teal-50/80 via-blue-50/50 to-transparent dark:from-teal-900/30 dark:via-blue-900/20 dark:to-transparent rounded-3xl -z-10"
     ></div>
@@ -25,10 +30,18 @@ const props = defineProps({
       ></div>
     </div>
     <h1
+      v-if="firstTagIsH1"
       class="heading-1 font-bold bg-gradient-to-r from-teal-500 to-blue-500 dark:from-teal-400 dark:to-blue-400 bg-clip-text text-transparent mb-4"
     >
       {{ title }}
     </h1>
+    <h2
+      v-else
+      class="heading-2 font-bold bg-gradient-to-r from-teal-500 to-blue-500 dark:from-teal-400 dark:to-blue-400 bg-clip-text text-transparent mb-4"
+    >
+      {{ title }}
+    </h2>
     <p class="text-lg">{{ description }}</p>
+    <slot name="default" />
   </section>
 </template>
