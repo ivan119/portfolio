58de62675db65a7d36006367a5816126af8f7b08
Your Name <you@example.com>
2025-09-19 14:09:34 +0200

    chore: remove inline tailwind since we capute all inside class (hover-main-gradiant)

--- components/BaseButton.vue
+++ components/BaseButton.vue
@@ -22,9 +22,7 @@ const buttonClasses = computed(() => [
   props.variant === "link-old"
     ? "font-bold decoration-2 decoration-gray-400 dark:decoration-gray-500 hover:underline hover:decoration-red-800 dark:hover:decoration-yellow-400 hover:bg-gradient-to-r hover:from-red-500 hover:to-orange-500 dark:hover:from-yellow-500 dark:hover:to-amber-400 hover:bg-clip-text hover:text-transparent"
     : "",
-  props.variant === "link"
-    ? "font-bold decoration-2 hover:underline hover:decoration-teal-600 hover:bg-gradient-to-r hover:from-teal-500 hover:to-blue-600 hover:bg-clip-text hover:text-transparent"
-    : "",
+  props.variant === "link" ? "font-bold hover-main-gradient" : "",
   props.variant === "default"
     ? "font-bold text-sm  py-1 rounded bg-gray-200  px-2 dark:bg-gray-800 hover:bg-gray-[250] dark:hover:bg-gray-700"
     : "",
