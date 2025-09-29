2315c2b2d9a437928d0a3c63e96da458728106d5
Your Name <you@example.com>
2025-09-29 15:47:36 +0200

    feat: composable for default layout

---
+++ composables/useDefaultLayout.ts
@@ -0,0 +1,115 @@
+export const useDefaultLayout = () => {
+  const route = useRoute();
+  const colorMode = useColorMode();
+  const isDark = computed(() => colorMode.value === "dark");
+
+  const transitionMode = ref<"slide" | "fade">("fade");
+  const transitionSlide = ref("slide-right");
+  const transitionFade = ref("page");
+
+  const typeWriterMode = ref(false);
+  const showIntro = useCookie<boolean>("showIntro", {
+    default: () => true,
+    watch: true,
+    sameSite: "lax",
+  });
+  const showMainContent = computed(() => !showIntro.value);
+  const showLogo = ref(!showIntro.value);
+
+  const changeState = (value: boolean) => {
+    showIntro.value = !value;
+  };
+
+  const updateShowLogo = (value: boolean) => {
+    showLogo.value = value;
+  };
+
+  const showIntroComponent = () => {
+    showLogo.value = false;
+    typeWriterMode.value = true;
+    setTimeout(() => {
+      showIntro.value = true;
+    }, 693);
+  };
+
+  const setupViewTransition = () => {
+    if ((document as any).startViewTransition) {
+      const handleNavigation = () => {
+        (document as any).startViewTransition();
+      };
+      window.addEventListener("popstate", handleNavigation);
+      onUnmounted(() => {
+        window.removeEventListener("popstate", handleNavigation);
+      });
+    }
+  };
+
+  onBeforeMount(() => {
+    typeWriterMode.value = false;
+    setupViewTransition();
+  });
+
+  const transitionSlideDirection = computed(() => {
+    return route.path === "/" ? "slide-left" : "slide-right";
+  });
+
+  const transition = computed(() => ({
+    name:
+      transitionMode.value === "slide"
+        ? transitionSlideDirection.value
+        : transitionFade.value,
+    mode: "out-in",
+  }));
+
+  const handleShowContent = () => {
+    if (route.path !== "/" && showIntro.value) {
+      showIntro.value = false;
+      updateShowLogo(true);
+    }
+  };
+
+  watch(
+    () => route.path,
+    () => {
+      handleShowContent();
+    },
+    { immediate: true },
+  );
+
+  watch(
+    [() => route.path, showIntro],
+    () => {
+      if (route.path !== "/" && showIntro.value) {
+        const router = useRouter();
+        router.push({ path: "/" });
+        setTimeout(() => {
+          showLogo.value = false;
+        }, 369);
+      }
+    },
+    { immediate: true },
+  );
+
+  const onIndexPage = computed(
+    () => route.name === "index" || route.path === "/",
+  );
+
+  return {
+    // state
+    isDark,
+    transitionMode,
+    transitionSlide,
+    transitionFade,
+    typeWriterMode,
+    showIntro,
+    showMainContent,
+    showLogo,
+    onIndexPage,
+    // computed
+    transition,
+    // methods
+    changeState,
+    updateShowLogo,
+    showIntroComponent,
+  };
+};
