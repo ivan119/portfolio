6663cdf71dd0abf57f8e5d1f26bff0bb14059160
Your Name <you@example.com>
2025-10-25 03:37:43 +0200

    chore: remove unused css

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,20 +1,3 @@
-<style scoped>
-/* this must be here otherwise it won't work as expected since it's still experimental*/
-:root {
-  --view-transition-duration: 369ms;
-}
-
-::view-transition-old(root),
-::view-transition-new(root) {
-  animation-duration: var(--view-transition-duration);
-}
-::view-transition-old(root) {
-  animation: 0.5s cubic-bezier(0.4, 0, 0.2, 1) both fade-out;
-}
-::view-transition-new(root) {
-  animation: 0.5s cubic-bezier(0.4, 0, 0.2, 1) both fade-in;
-}
-</style>
 <script setup lang="ts">
 import { useThemeButtons } from "~/composables/UI/useThemeButtons";
 import { useDefaultLayout } from "~/composables/useDefaultLayout";
