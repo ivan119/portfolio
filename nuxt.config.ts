92a46227b55cfb931d3996d586e90f1a31bfff55
Ivan Kelava <ivankelava991@gmail.com>
2026-02-14 04:57:33 +0100

    chore: add latest compatibilityDate to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
       ],
     },
   },
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: "latest",
   experimental: {
     viewTransition: true,
     renderJsonPayloads: false,
