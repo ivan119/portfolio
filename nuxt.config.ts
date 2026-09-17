ae44a91a2b32f4890c4a726b2dccb7c7a1785cec
Ivan Kelava <ivankelava991@gmail.com>
2026-09-17 15:12:51 +0200

    fix:?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,7 +50,7 @@ export default defineNuxtConfig({
     "@nuxtjs/color-mode",
     "@nuxt/image",
     "@vueuse/nuxt",
-    "@vercel/speed-insights",
+    "@vercel/speed-insights"
   ],
   postcss: {
     plugins: {
